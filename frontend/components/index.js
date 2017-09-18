873697492a0a5a4bf2ccdc947c4d6fffd0a93152
kinyusui <kaixngchen@gmail.com>
2017-09-18 16:03:36 -0400

    Basic Set Up

---
+++ frontend/components/index.js
@@ -0,0 +1,8 @@
+class Car {
+  manufacturer(car) {
+    document.write(`I have a ${car}`)
+  }
+}
+
+const bmw = new Car;
+bmw.manufacturer('bmw');
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const webpack = require('webpack');
+const path = require('path');
+
+let config = {
+  entry: './frontend/components/index.js',
+  output: {
+    path: path.join(__dirname, 'frontend/public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/, // files ending with .js
+      exclude: /node_modules/, // exclude the node_modules directory
+      loader: "babel-loader" // use this (babel-core) loader
+    }]
+  }
+}
+
+module.exports = config;
