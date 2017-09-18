5ed60d92a31e572321ea31fa539ea1ef6ca32efb
Lucas Ashby <lfeashby@Lucass-MacBook-Pro-2.local>
2017-09-18 19:37:11 -0400

    Lesson and LessonSlideListEntry rudimentary functionality implemented

---
+++ frontend/components/Lesson.js
@@ -0,0 +1,23 @@
+// HTML structure is very rudimentary. Should perhaps split title and description into a div and have the list separate.
+
+import React from 'react';
+import LessonSlideListEntry from './LessonSlideListEntry.js';
+
+const Lesson = (props) => (
+  <div className="lesson">
+    <h1 className="lessonTitle">{props.lesson.title}</h1>
+    <p className="lessonDescription">{props.lesson.description}</p>
+    <ol className="lessonOrderedList">
+      {props.slides.map((slide, i) => {
+        return <LessonSlideListEntry
+          slide={slide}
+          index={i}
+          key={i}
+          onLessonSlideListEntryClick={props.onLessonSlideListEntryClick}
+        />
+      })}
+    </ol>
+  </div>
+);
+
+export default Lesson;
