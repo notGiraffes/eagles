cb524480636c3d9c58d702d554967092ff5ea55b
ccolin84 <ccolin84@gmail.com>
2017-09-18 19:47:32 -0400

    basic navbar and routing

---
+++ frontend/components/NavBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+const NavBar = (props) => (
+  <div>
+    <span><Link to='Link1'>Link   |  </Link></span>
+    <span><Link to='Link2'>Link2  |  </Link></span>
+    <span><Link to='Link3'>Link3  |  </Link></span>
+    <span><Link to='Link4'>Link4  |  </Link></span>
+    <hr/>
+  </div>
+);
+
+export default NavBar;
