106459990a209fe5cebd74d4b30b6f84d40c32de
Lucas Ashby <lfeashby@Lucass-MacBook-Pro-2.local>
2017-09-19 14:23:47 -0400

    Lesson and slide basic functionality

--- frontend/components/Lesson.js
+++ frontend/components/Lesson.js
@@ -1,56 +1,20 @@
 import React from 'react';
 import LessonSlideListEntry from './LessonSlideListEntry.js';
 import Slide from './Slide.js';
-<<<<<<< HEAD
-
-=======
 // import lessons from '../testing/dummyData.js'
 
-var slides = [{
-  "name": "slide1",
-  "youTubeUrl": "lAJWHHUz8_8",
-  "text": "This is text for slide1",
-  "quizUrl": "quiz for slide1"
-},{
-  name: 'slide2',
-  youTubeUrl: "4ZAEBxGipoA",
-  text: "This is text for slide2",
-  quizUrl: "quiz for slide2"
-},{
-  name: 'slide3',
-  youTubeUrl: "mFEoarLnnqM",
-  text: "This is text for slide3",
-  quizUrl: "quiz for slide3"
-}];
 
-var lesson = {
-  name: 'lesson1',
-  description: 'lesson1 description',
-  slides: slides
-};
-
->>>>>>> lessonAndSlide
 class Lesson extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-<<<<<<< HEAD
-      currentSlide: '',
-      slides: props.slides,
-=======
       specificLesson:'',
       slides: '',
       currentSlide: '',
->>>>>>> lessonAndSlide
       index: ''
     }
   }
 
-<<<<<<< HEAD
-  onLessonSlideListEntryClick(index) {
-    this.setState({
-      currentSide: this.state.slides[index],
-=======
   componentDidMount() {
     return fetch('/lessons', { method: 'GET' })
       .then((response) => response.json())
@@ -70,19 +34,12 @@ class Lesson extends React.Component {
   onLessonSlideListEntryClick(index) {
     this.setState({
       currentSlide: this.state.slides[index],
->>>>>>> lessonAndSlide
       index: index
     });
   }
 
   previousSlideClick(index) {
     index--;
-<<<<<<< HEAD
-    this.setState({
-      index: index
-    });
-    onLessonSlideListEntryClick(index);
-=======
     if (index < 0) {
       alert("There is no previous slide! You will be redirected to the Lesson Home Page.");
       this.exit();
@@ -92,17 +49,10 @@ class Lesson extends React.Component {
       });
       this.onLessonSlideListEntryClick(index);
     }
->>>>>>> lessonAndSlide
   }
 
   nextSlideClick(index) {
     index++;
-<<<<<<< HEAD
-    this.setState({
-      index: index
-    });
-    onLessonSlideListEntryClick(index);
-=======
     if (index === this.state.slides.length) {
       alert('You\'ve made it to the end of the lesson.')
       this.exit();
@@ -119,7 +69,6 @@ class Lesson extends React.Component {
       currentSlide: '',
       index: ''
     });
->>>>>>> lessonAndSlide
   }
 
 
@@ -129,39 +78,23 @@ class Lesson extends React.Component {
         { this.state.currentSlide ? (
           <Slide 
           slideData={this.state.currentSlide} 
-<<<<<<< HEAD
-          previousSlideClick={props.previousSlideClick.bind(this)}
-          nextSlideClick={props.nextSlideClick.bind(this)}
-=======
           previousSlideClick={this.previousSlideClick.bind(this)}
           nextSlideClick={this.nextSlideClick.bind(this)}
           exitClick={this.exit.bind(this)}
           index={this.state.index}
->>>>>>> lessonAndSlide
           />
         ) : (
           <div className="lessonSlideList">
             <div className="lesson">
-<<<<<<< HEAD
-              <h1 className="lessonTitle">{props.lesson.name}</h1>
-              <p className="lessonDescription">{props.lesson.description}</p>
-              <ol className="lessonOrderedList">
-                {props.slides.map((slide, i) => {
-=======
               <h1 className="lessonTitle">{this.state.specificLesson.name}</h1>
               <p className="lessonDescription">{this.state.specificLesson.description}</p>
               <ol className="lessonOrderedList">
                 {this.state.slides.map((slide, i) => {
->>>>>>> lessonAndSlide
                   return <LessonSlideListEntry
                     slide={slide}
                     index={i}
                     key={i}
-<<<<<<< HEAD
-                    onLessonSlideListEntryClick={props.onLessonSlideListEntryClick.bind(this)}
-=======
                     onLessonSlideListEntryClick={this.onLessonSlideListEntryClick.bind(this)}
->>>>>>> lessonAndSlide
                   />
                 })}
               </ol>
@@ -174,11 +107,4 @@ class Lesson extends React.Component {
 }
 
 
-<<<<<<< HEAD
 export default Lesson;
-
-// previousSlide -  
-// currentSlide - 
-=======
-export default Lesson;
->>>>>>> lessonAndSlide
--- frontend/components/Slide.js
+++ frontend/components/Slide.js
@@ -1,40 +1,22 @@
-<<<<<<< HEAD
-const Slide = (props) => {
-	<div>
-    <h1>THIS IS THE SLIDE COMPONENT</h1>
-    <div className="youtubeVideoContainer">
-      <iframe className="youtubeVideo" src={props.slideData.youTubeUrl} allowFullScreen></iframe>
-    </div>
-    <div> 
-      The quiz URL should go here.
-=======
 import React from 'react';
 
 const Slide = (props) => (
-	<div>
+  <div>
     <h1>THIS IS THE SLIDE COMPONENT</h1>
     <div className="youtubeVideoContainer">
       <iframe className="youtubeVideo" src={'https://www.youtube.com/embed/' + props.slideData.youTubeUrl} allowFullScreen></iframe>
     </div>
     <div> 
         <p>{props.slideData.quizUrl}</p>
->>>>>>> lessonAndSlide
     </div>
     <div>
       <p>{props.slideData.text}</p>
     </div>
     <button type="button" onClick={() => props.previousSlideClick(props.index)}>Previous Slide</button>
     <button type="button" onClick={() => props.nextSlideClick(props.index)}>Next Slide</button>
-<<<<<<< HEAD
-    <button type="button">Exit</button>
-  </div>
-
-}
-=======
     <button type="button" onClick={() => props.exitClick()}>Exit</button>
   </div>
 
 );
->>>>>>> lessonAndSlide
 
 export default Slide;
