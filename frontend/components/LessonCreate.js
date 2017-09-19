87f2c5ed74292cc5e2452a6eb91b3d8a108f14f2
kinyusui <kaixngchen@gmail.com>
2017-09-19 13:54:03 -0400

    lessonCreate start

---
+++ frontend/components/LessonCreate.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import axios from 'axios';//use in functions
+
+class LessonCreate extends React.Component {
+  constructor (props) {
+    super(props);
+  }
+  render () {
+    return (
+      <div className='lessonCreate'>
+        <form>
+          <input/>
+        </form>
+      </div>
+    )
+  }   
+}
