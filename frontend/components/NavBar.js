36e58bb27c3f6e58cdf027c8a35a20f6ca950afc
Lucas Ashby <lfeashby@Lucass-MacBook-Pro-2.local>
2017-09-19 15:46:41 -0400

    Added rudimentary search functionality and comments

--- frontend/components/NavBar.js
+++ frontend/components/NavBar.js
@@ -9,15 +9,26 @@ class NavBar extends Component {
     }
   }
 
+  retrieveSearchInput(searchInput) {
+    this.setState({
+      searchInput: searchInput.target.value
+    });
+  }
+
+
   handleSearchSubmit(event) {
     event.preventDefault();
-    console.log('You clicked the button');
+    // Query server for info from database that matches this.state.searchInput
+      // Need to add keywords or some criteria to filter database by
   }
 
   render(props) {
     return (
       <div>
-        <span><input type='text' placeholder='Enter a search term!'/> <button>Search</button>      </span>
+        <span>
+          <input type='text' placeholder='Enter a search term!' onChange={this.retrieveSearchInput.bind(this)}/> 
+          <button onClick={this.handleSearchSubmit.bind(this)}>Search</button>      
+        </span>
         <span><Link to='/lesson/0'>LessonTester   |  </Link></span>
         <span><Link to='Link2'>Link2  |  </Link></span>
         <hr/>
@@ -27,3 +38,6 @@ class NavBar extends Component {
 }
 
 export default NavBar;
+
+
+// Can change input and button to elements within a form field which should mean you can submit by hitting enter as well as clicking submit
