af459dfad5033d2c382500711496f7ca4f0c5f4b
Dustin <dustykim@gmail.com>
2017-09-20 10:54:34 -0400

    revised put requests for new fields

--- backend/router.js
+++ backend/router.js
@@ -178,7 +178,7 @@ router.put('/lessons', function(req, res) {
     if (err) res.status(400).send(err);
 
     if (req.body.name) lesson.name = req.body.name;
-    if (req.body.createdBy) lesson.createdBy = req.body.createdBy;
+    if (req.body.userRef) lesson.userRef = req.body.userRef;
     if (req.body.description) lesson.description = req.body.description;
     if (req.body.slides) lesson.slides = req.body.slides;
 
@@ -194,10 +194,12 @@ router.put('/slides', function(req, res) {
     if (err) res.status(400).send(err);
 
     if (req.body.name) slide.name = req.body.name;
+    if (req.body.lessonRef) slide.lessonRef = req.body.lessonRef;
     if (req.body.youTubeUrl) slide.youTuleUrl = req.body.youTubeUrl;
     if (req.body.text) slide.text = req.body.text;
     if (req.body.quizUrl) slide.quizUrl = req.body.quizUrl;
-    if (req.body.fromLesson) slide.fromLesson = req.body.fromLesson;
+    if (req.body.youTubeThumbnailUrl) slide.youTubeThumbnailUrl = req.body.youTubeThumbnailUrl;
+    if (req.body.youTubeTags) slide.youTubeTags = req.body.youTubeTags;
 
     Slide.save(function (err) {
       if (err) res.send(err);
