63e2543d4f36f870a9445dd08cdd7cb943131463
ccolin84 <ccolin84@gmail.com>
2017-09-20 17:26:25 -0400

    changed logout

---
+++ backend/checkAuth.js
@@ -0,0 +1,38 @@
+const schema = require('./schema.js');
+let User = schema.User;
+
+exports.attemptLoggin = (req, res) => {
+  let username = req.body.username || '';
+  let password = req.body.password || '';
+  console.log('attempting loggin with: ', username, password);
+  // query db for user with password
+  User.find({ username: username, password: password  })
+    .then((users) => {
+      console.log('found users', users);
+      // if yes, estabish a session
+      if(users.length === 0) throw new Error('no Users');
+      let user = users[0];
+      req.session.user = true;
+      res.send({
+        loggedIn: true,
+        userData: user
+      })
+    })
+    .catch((err) => {
+      res.send({ loggedIn: false })
+    });
+}
+
+exports.createAccount = (req, res) => {
+  // validate the input
+  // create a user in the DB
+}
+
+exports.checkUser = (req, res, next) => {
+  // make sure the person making requests is logged in
+  if (!req.session.user) {
+    res.redirect('/logout');
+  } else {
+    next();
+  }
+}
--- backend/server.js
+++ backend/server.js
@@ -9,15 +9,30 @@ const router = require('./router.js');
 const database = require('./database.js');
 const path = require('path');
 const morgan = require('morgan');
+const checkAuth = require('./checkAuth');
+const session = require('express-session');
+
+app.set('trust proxy', 1) // trust first proxy 
+app.use(session({
+  secret: 'super secret',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
 
 
+app.use(bodyparser.json());
 app.use(morgan('tiny'));
 
 app.use(express.static('../frontend/public'));
 
+app.get('/logout', (req, res) => res.redirect('/'));
+app.post('/createAccount', checkAuth.createAccount);
+app.post('/login', checkAuth.attemptLoggin);
+app.use(checkAuth.checkUser);
+
 app.set('port', (process.env.PORT || 3000));
 
-app.use(['/','/slides','/lessons','tutorials'], bodyparser.json());
 app.use(['/','/slides','/lessons','tutorials'], router);
 
 app.get('*', (req, res) => {
