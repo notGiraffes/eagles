66827577513c4b32affca38c46ddb05fb30c2460
Lucas Ashby <lfeashby@Lucass-MacBook-Pro-2.local>
2017-09-21 16:18:37 -0400

    Added thumbnails to slide and basic styling

--- frontend/components/Lesson/LessonSlideListEntry.js
+++ frontend/components/Lesson/LessonSlideListEntry.js
@@ -1,8 +1,28 @@
 import React from 'react';
 
 const LessonSlideListEntry = (props) => (
-  <li className="LessonSlideListEntry" onClick={() => props.onLessonSlideListEntryClick(props.index)}>{props.slide.name}</li>
+  <div className="LessonSlideListEntry" onClick={() => props.onLessonSlideListEntryClick(props.index)}>
+    <h4 className="index">{props.index + 1}</h4>
+    <img className="thumbnail" src={props.slide.youTubeThumbnailUrl} alt="" />
+    <span className="slideNameInLessonSlideListEntry">
+       {props.slide.name}
+    </span>
+  </div>
 );
 
 
 export default LessonSlideListEntry;
+
+
+
+
+// const LessonSlideListEntry = (props) => (
+//   <div className="LessonSlideListEntry" onClick={() => props.onLessonSlideListEntryClick(props.index)}>
+//     <div>
+//       {props.index + 1}
+//     </div>
+//     <div>
+//       <img src={props.slide.youTubeThumbnailUrl} alt="" />
+//     </div>
+//     <p>{props.slide.name}</p>
+//   </div>
