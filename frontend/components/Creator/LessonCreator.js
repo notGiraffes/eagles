ce042a3edd08dcddc97e63326a21b32f10801b92
kinyusui <kaixngchen@gmail.com>
2017-09-21 10:51:17 -0400

    merge with lucas

--- frontend/components/Creator/LessonCreator.js
+++ frontend/components/Creator/LessonCreator.js
@@ -45,6 +45,7 @@ class LessonCreator extends React.Component {
   }
   keyWordSubmit (event) {
     event.preventDefault();
+<<<<<<< HEAD
     console.log('keyWordSubmit triggered');
     var body = {keyWords: this.state.keyWords, lessonid: this.state.lessonid};
     axios.put('/lessons', body)
@@ -57,6 +58,13 @@ class LessonCreator extends React.Component {
     arr.push(event.target.value);
     this.setState({
       keyWords: arr
+=======
+    console.log('keyWordSubmit triggered')
+  }
+  changeKeyWords (event) {
+    this.setState({
+      keyWords: event.target.value
+>>>>>>> working on put
     })
   }
   changeName (event) {
