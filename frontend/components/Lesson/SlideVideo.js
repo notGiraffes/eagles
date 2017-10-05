0799cc0797f00374a8f4b927cb42580c76dd5ace
Simon K Zheng <simonkzheng@gmail.com>
2017-10-05 13:17:01 -0400

    test

--- frontend/components/Lesson/SlideVideo.js
+++ frontend/components/Lesson/SlideVideo.js
@@ -2,7 +2,6 @@ import React from 'react';
 import axios from 'axios';
 import ReactPlayer from 'react-player';
 import $ from 'jquery';
-import Annotations from './Annotations.js';
 
 class SlideVideo extends React.Component {
   constructor(props){
